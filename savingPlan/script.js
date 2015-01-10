d5e58d3e5aa7153693a1eadc1d8cad7bb38a387a
SupaStuff <StuffQilla@gmail.com>
2015-01-10 16:23:37 -0500

    retroSavingPlan added

--- savingPlan/script.js
+++ savingPlan/script.js
@@ -5,18 +5,23 @@ $(document).ready(function(){
 	var week = Math.ceil(diff_ms/(1000*60*60*24*7))+1;
 	$('#weekNo').text(week);
 	
+	var retroWeek = 53-week;
+	
 	var savings = 0;
-	
+	var retroSavings = 0;
 	for(var i = 0; i<=week; i++) savings += i;
-	
-	var actual = Number(document.getElementById('actual').value);
+	for(var i = 52; i>=retroWeek; i--) retroSavings += i;
 	
 	for(var i = 1; i<=4; i++)
 	{
 		$('#deposit'+i).text("$"+week*i);
 		$('#savings'+i).text("$"+savings*i);
-		$('#actual'+i).text("$"+(actual+(week*i)));
+		
+		$('#retrodeposit'+i).text("$"+retroWeek*i);
+		$('#retrosavings'+i).text("$"+retroSavings*i);
 	}
+	
+	
 });
 
 $(document).on('change', '#actual', function(){
@@ -24,4 +29,11 @@ $(document).on('change', '#actual', function(){
 	var actual = Number(document.getElementById('actual').value);
 	
 	for(var i = 1; i<=4; i++) $('#actual'+i).text("$"+(actual+(week*i)));
+});
+
+$(document).on('change', '#retroactual', function(){
+	var week = 53-Number(document.getElementById('weekNo').innerHTML);
+	var actual = Number(document.getElementById('retroactual').value);
+	
+	for(var i = 1; i<=4; i++) $('#retroactual'+i).text("$"+(actual+(week*i)));
 });
